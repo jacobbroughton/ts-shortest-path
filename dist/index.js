a7b536de935bf3362fbb9c2b296980de9f959a2f
Jacob Broughton <jlbroughton88@gmail.com>
2025-03-09 08:23:59 -0400

    added constraint that above,left,right,below coord must be on same alternate axis

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,23 @@
+const maxDimensions = [10, 10];
+const currCoords = [0, 0];
+const targetCoords = [
+    Math.floor(Math.random() * maxDimensions[0]),
+    Math.floor(Math.random() * maxDimensions[1]),
+];
+let currAxisIndex = 0; // 0 = x, 1 = y
+let selectedCoords = [null, null];
+let aboveCoord = null;
+let belowCoord = null;
+let leftCoord = null;
+let rightCoord = null;
+let running = true;
+let needsRefresh = false;
+let surroundingsNeedRefresh = false;
 const gridElement = document.querySelector(".grid");
 gridElement.style.width = "300px";
 gridElement.style.height = "300px";
 gridElement.style.backgroundColor = "grey";
 gridElement.style.position = "relative";
-const maxDimensions = [10, 10];
-const currCoords = [0, 0];
-let currAxisIndex = 0; // 0 = x, 1 = y
-let running = true;
-let needsRefresh = false;
-const targetCoords = [
-    Math.floor(Math.random() * maxDimensions[0]),
-    Math.floor(Math.random() * maxDimensions[1]),
-];
-let selectedCoords = [null, null];
 // render grid
 while (running) {
     // both limits are reached, exit loop
@@ -36,13 +41,14 @@ while (running) {
     nodeElement.dataset.coordY = `${currCoords[1]}`;
     nodeElement.addEventListener("click", (e) => {
         const el = e.target;
-        const clickedCoords = [parseInt(el.dataset.coordX), parseInt(el.dataset.coordY)];
-        if (clickedCoords[0] === targetCoords[0] && clickedCoords[1] === targetCoords[1])
+        selectedCoords = [parseInt(el.dataset.coordX), parseInt(el.dataset.coordY)];
+        if (selectedCoords[0] === targetCoords[0] && selectedCoords[1] === targetCoords[1])
             return;
-        nodeElement.style.backgroundColor = "blue";
-        selectedCoords[0] = clickedCoords[0];
-        selectedCoords[1] = clickedCoords[1];
+        nodeElement.style.backgroundColor = "brown";
+        selectedCoords[0] = selectedCoords[0];
+        selectedCoords[1] = selectedCoords[1];
         needsRefresh = true;
+        surroundingsNeedRefresh = true;
         refreshGrid();
     });
     gridElement.appendChild(nodeElement);
@@ -58,78 +64,139 @@ while (running) {
         currAxisIndex = 0;
 }
 function refreshGrid() {
-    const refreshCoords = [0, 0];
+    const currentRefreshCoords = [0, 0];
     let currRefreshAxisIndex = 0;
     let runIndex = 0;
     while (needsRefresh) {
         if (currRefreshAxisIndex === 1 &&
-            refreshCoords[currRefreshAxisIndex] >= maxDimensions[currRefreshAxisIndex]) {
+            currentRefreshCoords[currRefreshAxisIndex] >= maxDimensions[currRefreshAxisIndex]) {
             needsRefresh = false;
             break;
         }
-        // determine surrounding coordinates (above, bottom, left, right)
-        let aboveCoord = null;
-        if (refreshCoords[1] > 0) {
-            const aboveX = refreshCoords[0];
-            const aboveY = refreshCoords[1] - 1;
+        if (!selectedCoords[0] || !selectedCoords[1])
+            throw new Error("No selected coords found");
+        // determine surrounding coordinates (above, below, left, right)
+        if (selectedCoords[1] - 1 === currentRefreshCoords[1] - 1 &&
+            selectedCoords[0] === currentRefreshCoords[0]) {
+            const aboveX = currentRefreshCoords[0];
+            const aboveY = currentRefreshCoords[1] - 1;
             aboveCoord = [aboveX, aboveY];
         }
-        let belowCoord = null;
-        if (refreshCoords[1] < maxDimensions[1] - 1) {
-            const belowX = refreshCoords[0];
-            const belowY = refreshCoords[1] + 1;
+        if (
+        // currentRefreshCoords[1] < maxDimensions[1] - 1 &&
+        selectedCoords[1] + 1 === currentRefreshCoords[1] + 1 &&
+            selectedCoords[0] === currentRefreshCoords[0]) {
+            const belowX = currentRefreshCoords[0];
+            const belowY = currentRefreshCoords[1] + 1;
             belowCoord = [belowX, belowY];
         }
-        let leftCoord = null;
-        if (refreshCoords[0] > 0) {
-            const leftX = refreshCoords[0] - 1;
-            const leftY = refreshCoords[1];
+        if (
+        // currentRefreshCoords[0] > 0 &&
+        selectedCoords[0] - 1 === currentRefreshCoords[0] - 1 &&
+            selectedCoords[1] === currentRefreshCoords[1]) {
+            const leftX = currentRefreshCoords[0] - 1;
+            const leftY = currentRefreshCoords[1];
+            console.log(leftX, leftY);
             leftCoord = [leftX, leftY];
         }
-        let rightCoord = null;
-        if (refreshCoords[0] < maxDimensions[0] - 1) {
-            const rightX = refreshCoords[0] + 1;
-            const rightY = refreshCoords[1];
+        if (
+        // currentRefreshCoords[0] < maxDimensions[0] - 1 &&
+        selectedCoords[0] + 1 === currentRefreshCoords[0] + 1 &&
+            selectedCoords[1] === currentRefreshCoords[1]) {
+            const rightX = currentRefreshCoords[0] + 1;
+            const rightY = currentRefreshCoords[1];
             rightCoord = [rightX, rightY];
         }
-        if (refreshCoords[0] === selectedCoords[0] &&
-            refreshCoords[1] === selectedCoords[1]) {
+        if (surroundingsNeedRefresh)
+            surroundingsNeedRefresh = false;
+        const isClickedSquare = currentRefreshCoords[0] === selectedCoords[0] &&
+            currentRefreshCoords[1] === selectedCoords[1];
+        if (isClickedSquare) {
             // determine which closest (on the grid, not numerically)
-            const leftDiff = [leftCoord[0] - targetCoords[0], leftCoord[1] - targetCoords[1]];
-            const rightDiff = [
-                rightCoord[0] - targetCoords[0],
-                rightCoord[1] - targetCoords[1],
-            ];
-            const aboveDiff = [
-                aboveCoord[0] - targetCoords[0],
-                aboveCoord[1] - targetCoords[1],
-            ];
-            const belowDiff = [
-                belowCoord[0] - targetCoords[0],
-                belowCoord[1] - targetCoords[1],
-            ];
-            console.log(`
-target: ${targetCoords.toString()}
-left: ${leftCoord?.toString()} - diff: ${leftDiff.toString()}
-right: ${rightCoord?.toString()} - diff: ${rightDiff.toString()}
-above: ${aboveCoord?.toString()} - diff: ${aboveDiff.toString()}
-below: ${belowCoord?.toString()} - diff: ${belowDiff.toString()}
-        `);
+            if (!targetCoords[0] || !targetCoords[1])
+                throw new Error("No target coordinates");
+            let leftDiff = [0, 0];
+            if (leftCoord && leftCoord[0] && leftCoord[1])
+                leftDiff = [leftCoord[0] - targetCoords[0], leftCoord[1] - targetCoords[1]];
+            let rightDiff = [0, 0];
+            if (rightCoord && rightCoord[0] && rightCoord[1])
+                rightDiff = [rightCoord[0] - targetCoords[0], rightCoord[1] - targetCoords[1]];
+            let aboveDiff = [0, 0];
+            if (aboveCoord && aboveCoord[0] && aboveCoord[1])
+                aboveDiff = [aboveCoord[0] - targetCoords[0], aboveCoord[1] - targetCoords[1]];
+            let belowDiff = [0, 0];
+            if (belowCoord && belowCoord[0] && belowCoord[1])
+                belowDiff = [belowCoord[0] - targetCoords[0], belowCoord[1] - targetCoords[1]];
+            // console.log(
+            //   `
+            //     selected: ${selectedCoords.toString()}
+            //     target: ${targetCoords.toString()}
+            //     left: ${leftCoord?.toString()} - diff: ${leftDiff.toString()}
+            //     right: ${rightCoord?.toString()} - diff: ${rightDiff.toString()}
+            //     above: ${aboveCoord?.toString()} - diff: ${aboveDiff.toString()}
+            //     below: ${belowCoord?.toString()} - diff: ${belowDiff.toString()}
+            //   `
+            // );
         }
-        const nodeElement = document.querySelector(`[data-coord-x="${refreshCoords[0]}"][data-coord-y="${refreshCoords[1]}"]`);
-        if (!(refreshCoords[0] === selectedCoords[0] && refreshCoords[1] === selectedCoords[1]) &&
-            !(refreshCoords[0] === targetCoords[0] && refreshCoords[1] === targetCoords[1])) {
+        const nodeElement = document.querySelector(`[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`);
+        if (!(currentRefreshCoords[0] === selectedCoords[0] &&
+            currentRefreshCoords[1] === selectedCoords[1]) &&
+            !(currentRefreshCoords[0] === targetCoords[0] &&
+                currentRefreshCoords[1] === targetCoords[1])) {
             nodeElement.style.backgroundColor = "green";
+            nodeElement.classList.remove("right");
+            nodeElement.classList.remove("left");
+            nodeElement.classList.remove("above");
+            nodeElement.classList.remove("below");
         }
+        if (rightCoord && rightCoord.length) {
+            const isRightSquare = currentRefreshCoords[0] === rightCoord[0] &&
+                currentRefreshCoords[1] === rightCoord[1];
+            if (isRightSquare) {
+                const rightNodeElement = document.querySelector(`[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`);
+                rightNodeElement.classList.add("right");
+                rightNodeElement.style.backgroundColor = "red";
+            }
+        }
+        if (leftCoord && leftCoord.length) {
+            const isLeftSquare = currentRefreshCoords[0] === leftCoord[0] &&
+                currentRefreshCoords[1] === leftCoord[1];
+            if (isLeftSquare) {
+                const leftNodeElement = document.querySelector(`[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`);
+                leftNodeElement.classList.add("left");
+                leftNodeElement.style.backgroundColor = "yellow";
+            }
+        }
+        if (aboveCoord && aboveCoord.length) {
+            const isAboveSquare = currentRefreshCoords[0] === aboveCoord[0] &&
+                currentRefreshCoords[1] === aboveCoord[1];
+            if (isAboveSquare) {
+                const aboveNodeElement = document.querySelector(`[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`);
+                aboveNodeElement.classList.add("above");
+                aboveNodeElement.style.backgroundColor = "apricot";
+            }
+        }
+        if (belowCoord && belowCoord.length) {
+            const isBelowSquare = currentRefreshCoords[0] === belowCoord[0] &&
+                currentRefreshCoords[1] === belowCoord[1];
+            if (isBelowSquare) {
+                const belowNodeElement = document.querySelector(`[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`);
+                belowNodeElement.classList.add("below");
+                belowNodeElement.style.backgroundColor = "blue";
+            }
+        }
+        // if (currentRefreshCoords[0] == aboveCoord[0] && currentRefreshCoords[1] == aboveCoord[1])
+        // console.log(currentRefreshCoords, 'above: ', aboveCoord, "below: ", belowCoord, "left: ", leftCoord, 'right: ', rightCoord);
         // max x reached, focus on y axis and reset x coord to 0
         if (currRefreshAxisIndex === 0 &&
-            refreshCoords[currRefreshAxisIndex] === maxDimensions[currRefreshAxisIndex] - 1) {
+            currentRefreshCoords[currRefreshAxisIndex] ===
+                maxDimensions[currRefreshAxisIndex] - 1) {
             currRefreshAxisIndex = 1;
-            refreshCoords[0] = 0;
+            currentRefreshCoords[0] = 0;
         }
         // increment coord on current axis
-        refreshCoords[currRefreshAxisIndex] += 1;
-        if (refreshCoords[0] === 0 && refreshCoords[1] < maxDimensions[1])
+        currentRefreshCoords[currRefreshAxisIndex] += 1;
+        if (currentRefreshCoords[0] === 0 && currentRefreshCoords[1] < maxDimensions[1])
             currRefreshAxisIndex = 0;
         runIndex++;
     }
--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
-const gridElement = document.querySelector(".grid") as HTMLDivElement;
-gridElement.style.width = "300px";
-gridElement.style.height = "300px";
-gridElement.style.backgroundColor = "grey";
-gridElement.style.position = "relative";
-
 const maxDimensions: [number, number] = [10, 10];
 const currCoords: [number, number] = [0, 0];
-let currAxisIndex: 0 | 1 = 0; // 0 = x, 1 = y
-let running = true;
-let needsRefresh = false;
 const targetCoords = [
   Math.floor(Math.random() * maxDimensions[0]),
   Math.floor(Math.random() * maxDimensions[1]),
 ]!;
 
+let currAxisIndex: 0 | 1 = 0; // 0 = x, 1 = y
 let selectedCoords: [number | null, number | null] = [null, null];
+let aboveCoord: null | [number, number] = null;
+let belowCoord: null | [number, number] = null;
+let leftCoord: null | [number, number] = null;
+let rightCoord: null | [number, number] = null;
+let running = true;
+let needsRefresh = false;
+let surroundingsNeedRefresh = false;
+
+const gridElement = document.querySelector(".grid") as HTMLDivElement;
+gridElement.style.width = "300px";
+gridElement.style.height = "300px";
+gridElement.style.backgroundColor = "grey";
+gridElement.style.position = "relative";
 
 // render grid
 while (running) {
@@ -42,16 +47,17 @@ while (running) {
 
   nodeElement.addEventListener("click", (e: MouseEvent) => {
     const el = e.target as HTMLButtonElement;
-    const clickedCoords = [parseInt(el.dataset.coordX!), parseInt(el.dataset.coordY!)];
+    selectedCoords = [parseInt(el.dataset.coordX!), parseInt(el.dataset.coordY!)];
 
-    if (clickedCoords[0] === targetCoords[0] && clickedCoords[1] === targetCoords[1])
+    if (selectedCoords[0] === targetCoords[0] && selectedCoords[1] === targetCoords[1])
       return;
 
-    nodeElement.style.backgroundColor = "blue";
+    nodeElement.style.backgroundColor = "brown";
 
-    selectedCoords[0] = clickedCoords[0]!;
-    selectedCoords[1] = clickedCoords[1]!;
+    selectedCoords[0] = selectedCoords[0]!;
+    selectedCoords[1] = selectedCoords[1]!;
     needsRefresh = true;
+    surroundingsNeedRefresh = true;
 
     refreshGrid();
   });
@@ -74,56 +80,70 @@ while (running) {
 }
 
 function refreshGrid() {
-  const refreshCoords: [number, number] = [0, 0];
+  const currentRefreshCoords: [number, number] = [0, 0];
   let currRefreshAxisIndex: 0 | 1 = 0;
-
   let runIndex = 0;
+
   while (needsRefresh) {
     if (
       currRefreshAxisIndex === 1 &&
-      refreshCoords[currRefreshAxisIndex] >= maxDimensions[currRefreshAxisIndex]
+      currentRefreshCoords[currRefreshAxisIndex] >= maxDimensions[currRefreshAxisIndex]
     ) {
       needsRefresh = false;
       break;
     }
 
-    // determine surrounding coordinates (above, bottom, left, right)
-    let aboveCoord = null;
+    if (!selectedCoords[0] || !selectedCoords[1])
+      throw new Error("No selected coords found");
 
-    if (refreshCoords[1] > 0) {
-      const aboveX = refreshCoords[0];
-      const aboveY = refreshCoords[1] - 1;
+    // determine surrounding coordinates (above, below, left, right)
+    if (
+      selectedCoords[1] - 1 === currentRefreshCoords[1] - 1 &&
+      selectedCoords[0] === currentRefreshCoords[0]
+    ) {
+      const aboveX = currentRefreshCoords[0];
+      const aboveY = currentRefreshCoords[1] - 1;
       aboveCoord = [aboveX, aboveY];
     }
 
-    let belowCoord = null;
-
-    if (refreshCoords[1] < maxDimensions[1] - 1) {
-      const belowX = refreshCoords[0];
-      const belowY = refreshCoords[1] + 1;
+    if (
+      // currentRefreshCoords[1] < maxDimensions[1] - 1 &&
+      selectedCoords[1] + 1 === currentRefreshCoords[1] + 1 &&
+      selectedCoords[0] === currentRefreshCoords[0]
+    ) {
+      const belowX = currentRefreshCoords[0];
+      const belowY = currentRefreshCoords[1] + 1;
       belowCoord = [belowX, belowY];
     }
 
-    let leftCoord = null;
-
-    if (refreshCoords[0] > 0) {
-      const leftX = refreshCoords[0] - 1;
-      const leftY = refreshCoords[1];
+    if (
+      // currentRefreshCoords[0] > 0 &&
+      selectedCoords[0] - 1 === currentRefreshCoords[0] - 1 &&
+      selectedCoords[1] === currentRefreshCoords[1]
+    ) {
+      const leftX = currentRefreshCoords[0] - 1;
+      const leftY = currentRefreshCoords[1];
+      console.log(leftX, leftY)
       leftCoord = [leftX, leftY];
     }
 
-    let rightCoord = null;
-
-    if (refreshCoords[0] < maxDimensions[0] - 1) {
-      const rightX = refreshCoords[0] + 1;
-      const rightY = refreshCoords[1];
+    if (
+      // currentRefreshCoords[0] < maxDimensions[0] - 1 &&
+      selectedCoords[0] + 1 === currentRefreshCoords[0] + 1 &&
+      selectedCoords[1] === currentRefreshCoords[1]
+    ) {
+      const rightX = currentRefreshCoords[0] + 1;
+      const rightY = currentRefreshCoords[1];
       rightCoord = [rightX, rightY];
     }
 
-    if (
-      refreshCoords[0] === selectedCoords[0] &&
-      refreshCoords[1] === selectedCoords[1]
-    ) {
+    if (surroundingsNeedRefresh) surroundingsNeedRefresh = false;
+
+    const isClickedSquare =
+      currentRefreshCoords[0] === selectedCoords[0] &&
+      currentRefreshCoords[1] === selectedCoords[1];
+
+    if (isClickedSquare) {
       // determine which closest (on the grid, not numerically)
 
       if (!targetCoords[0] || !targetCoords[1]) throw new Error("No target coordinates");
@@ -148,43 +168,119 @@ function refreshGrid() {
       if (belowCoord && belowCoord[0] && belowCoord[1])
         belowDiff = [belowCoord[0] - targetCoords[0], belowCoord[1] - targetCoords[1]];
 
-      console.log(
-        `
-target: ${targetCoords.toString()}
-left: ${leftCoord?.toString()} - diff: ${leftDiff.toString()}
-right: ${rightCoord?.toString()} - diff: ${rightDiff.toString()}
-above: ${aboveCoord?.toString()} - diff: ${aboveDiff.toString()}
-below: ${belowCoord?.toString()} - diff: ${belowDiff.toString()}
-        `
-      );
+      // console.log(
+      //   `
+      //     selected: ${selectedCoords.toString()}
+      //     target: ${targetCoords.toString()}
+      //     left: ${leftCoord?.toString()} - diff: ${leftDiff.toString()}
+      //     right: ${rightCoord?.toString()} - diff: ${rightDiff.toString()}
+      //     above: ${aboveCoord?.toString()} - diff: ${aboveDiff.toString()}
+      //     below: ${belowCoord?.toString()} - diff: ${belowDiff.toString()}
+      //   `
+      // );
     }
 
     const nodeElement = document.querySelector(
-      `[data-coord-x="${refreshCoords[0]}"][data-coord-y="${refreshCoords[1]}"]`
+      `[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`
     ) as HTMLButtonElement;
 
     if (
       !(
-        refreshCoords[0] === selectedCoords[0] && refreshCoords[1] === selectedCoords[1]
+        currentRefreshCoords[0] === selectedCoords[0] &&
+        currentRefreshCoords[1] === selectedCoords[1]
       ) &&
-      !(refreshCoords[0] === targetCoords[0] && refreshCoords[1] === targetCoords[1])
+      !(
+        currentRefreshCoords[0] === targetCoords[0] &&
+        currentRefreshCoords[1] === targetCoords[1]
+      )
     ) {
       nodeElement.style.backgroundColor = "green";
+      nodeElement.classList.remove("right");
+      nodeElement.classList.remove("left");
+      nodeElement.classList.remove("above");
+      nodeElement.classList.remove("below");
     }
+
+    if (rightCoord && rightCoord.length) {
+      const isRightSquare =
+        currentRefreshCoords[0] === rightCoord[0] &&
+        currentRefreshCoords[1] === rightCoord[1];
+
+      if (isRightSquare) {
+        const rightNodeElement = document.querySelector(
+          `[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`
+        ) as HTMLButtonElement;
+
+        rightNodeElement.classList.add("right");
+
+        rightNodeElement.style.backgroundColor = "red";
+      }
+    }
+
+    if (leftCoord && leftCoord.length) {
+      const isLeftSquare =
+        currentRefreshCoords[0] === leftCoord[0] &&
+        currentRefreshCoords[1] === leftCoord[1];
+
+      if (isLeftSquare) {
+        const leftNodeElement = document.querySelector(
+          `[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`
+        ) as HTMLButtonElement;
+
+        leftNodeElement.classList.add("left");
+
+        leftNodeElement.style.backgroundColor = "yellow";
+      }
+    }
+
+    if (aboveCoord && aboveCoord.length) {
+      const isAboveSquare =
+        currentRefreshCoords[0] === aboveCoord[0] &&
+        currentRefreshCoords[1] === aboveCoord[1];
+
+      if (isAboveSquare) {
+        const aboveNodeElement = document.querySelector(
+          `[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`
+        ) as HTMLButtonElement;
+
+        aboveNodeElement.classList.add("above");
+
+        aboveNodeElement.style.backgroundColor = "apricot";
+      }
+    }
+
+    if (belowCoord && belowCoord.length) {
+      const isBelowSquare =
+        currentRefreshCoords[0] === belowCoord[0] &&
+        currentRefreshCoords[1] === belowCoord[1];
+
+      if (isBelowSquare) {
+        const belowNodeElement = document.querySelector(
+          `[data-coord-x="${currentRefreshCoords[0]}"][data-coord-y="${currentRefreshCoords[1]}"]`
+        ) as HTMLButtonElement;
+
+        belowNodeElement.classList.add("below");
+        belowNodeElement.style.backgroundColor = "blue";
+      }
+    }
+
+    // if (currentRefreshCoords[0] == aboveCoord[0] && currentRefreshCoords[1] == aboveCoord[1])
+    // console.log(currentRefreshCoords, 'above: ', aboveCoord, "below: ", belowCoord, "left: ", leftCoord, 'right: ', rightCoord);
 
     // max x reached, focus on y axis and reset x coord to 0
     if (
       currRefreshAxisIndex === 0 &&
-      refreshCoords[currRefreshAxisIndex] === maxDimensions[currRefreshAxisIndex] - 1
+      currentRefreshCoords[currRefreshAxisIndex] ===
+        maxDimensions[currRefreshAxisIndex] - 1
     ) {
       currRefreshAxisIndex = 1;
-      refreshCoords[0] = 0;
+      currentRefreshCoords[0] = 0;
     }
 
     // increment coord on current axis
-    refreshCoords[currRefreshAxisIndex] += 1;
+    currentRefreshCoords[currRefreshAxisIndex] += 1;
 
-    if (refreshCoords[0] === 0 && refreshCoords[1] < maxDimensions[1])
+    if (currentRefreshCoords[0] === 0 && currentRefreshCoords[1] < maxDimensions[1])
       currRefreshAxisIndex = 0;
 
     runIndex++;
